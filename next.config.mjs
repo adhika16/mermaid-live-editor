899b53062d0646338c22fbe3a0f1e452923915ba
Adhika P. <40765693+adhika16@users.noreply.github.com>
2025-10-27 17:00:14 +0700

    feat: refactor OAuth URL handling to use environment variable for app URL

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	env: {
+		NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
 		APPWRITE_ENDPOINT: process.env.APPWRITE_ENDPOINT,
 		APPWRITE_PROJECT_ID: process.env.APPWRITE_PROJECT_ID,
 		APPWRITE_DATABASE_ID: process.env.APPWRITE_DATABASE_ID,
